751f86d0c96e723137af28c6c072d4e9d352ab4e
digital-brew <rafal+github@digitalbrew.io>
2024-11-03 06:14:06 +0000

    Add block icon, description, update category.

--- resources/scripts/editor/block-register.js
+++ resources/scripts/editor/block-register.js
@@ -3,13 +3,14 @@ import {Fragment} from '@wordpress/element';
 import BlockOptions from "@scripts/editor/components/block-options";
 import BlockFields from "@scripts/editor/components/block-fields";
 
-if ('undefined' !== typeof gutengoodBlocks) {
-  gutengoodBlocks.forEach(block => {
+if ('undefined' !== typeof window.gutengoodBlocks) {
+  window.gutengoodBlocks.forEach(block => {
     const name = block.name;
     registerBlockType(name, {
       title: block.title,
-      icon: 'block-default',
-      category: 'common',
+      icon: getIcon ( block.icon ),
+      description: block.description,
+      category: block.category,
       edit: (props) => {
         return (
             <Fragment>
@@ -22,3 +23,28 @@ if ('undefined' !== typeof gutengoodBlocks) {
     });
   });
 }
+
+function getIcon ( icon ) {
+  console.log ( icon );
+  if ( !icon.includes ( '"svg"' ) ) return icon;
+  const json = JSON.parse ( icon );
+  // if ( !icon.hasOwnProperty ( 'svg' ) ) return icon;
+  console.log ( json );
+  const { svg } = json;
+  const paths = svg.paths
+    ? svg.paths.map ( ( path ) => wp.element.createElement ( 'path', {
+      fill: path[ '@attributes' ].fill,
+      d: path[ '@attributes' ].d,
+    } ) )
+    : wp.element.createElement ( 'path', {
+      fill: svg.path[ '@attributes' ].fill,
+      d: svg.path[ '@attributes' ].d,
+    } );
+
+  return wp.element.createElement ( 'svg', {
+    width: svg[ '@attributes' ].width,
+    height: svg[ '@attributes' ].height,
+    viewBox: svg[ '@attributes' ].viewBox,
+  }, paths );
+}
+
